fdebf47f85eb3b612d162837e9a9297ebf7c235f
pyaephyoaung1312000 <114916416+pyaephyoaung1312000@users.noreply.github.com>
2022-10-03 18:02:56 +0630

    Update config.py

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ import os
 API_ID = int(os.getenv("18274369"))
 API_HASH = os.getenv("a80ce95e803f9e448db2dba5ab75f0dd")
 BOT_TOKEN = os.getenv("5458345105:AAHAEmrT2EDiVg6T8r_Rqjd2E1ypEE_xtG4")
-DATABASE_URL = os.getenv("YouTube.com")
-OWNER_ID = list({int(x) for x in os.environ.get("OWNER_ID", "5465001197").split()})
+DATABASE_URL = os.getenv("youtube.com")
+OWNER_ID = list({int(x) for x in os.environ.get("5465001197").split()})
